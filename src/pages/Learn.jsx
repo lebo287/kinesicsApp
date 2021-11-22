f505f77d0f554f9f8886da2338e2670a005cecbb
hardy lutula <hardy@thedigitalacademy.co.za>
2021-11-22 13:06:30 +0200

    ..

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Learn = () => {
     return (
         <div>
-            <h1>Learn</h1>
+            <div className="center-layout">
+      <div className="auth-card animate__animated animate__fadeInUp animate__faster">
+        <div className="header">Learn</div>
+
+        
+      </div>
+    </div>
         </div>
     )
 }
